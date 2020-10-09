8c752255cd98d066ea620df26f36d6899955f17d
hrissolo <hrissolo@gmail.com>
2020-10-09 15:33:11 -0500

    working on tasks

---
+++ src/tasks/TaskProvider.js
@@ -0,0 +1,69 @@
+import React, { useState, createContext } from "react"
+
+/*
+    The context is imported and used by individual components
+    that need data
+*/
+export const TaskContext = createContext()
+
+/*
+ This component establishes what data can be used.
+ */
+export const TaskProvider = (props) => {
+    const [tasks, setTasks] = useState([])
+
+    const getTasks = () => {
+        return fetch("http://localhost:8088/tasks")
+            .then(res => res.json())
+            .then(setTasks)
+    }
+
+    const getTaskById = (id) => {
+        return fetch(`http://localhost:8088/tasks/${id}`)
+            .then(res => res.json())
+    }
+    
+
+    const addTask = id => {
+        return fetch("http://localhost:8088/tasks", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(id)
+        })
+            .then(getTasks)
+    }
+
+    const deleteTask = taskId => {
+        return fetch(`http://localhost:8088/tasks/${taskId}`, {
+            method: "DELETE"
+        })
+            .then(getTasks)
+    }    
+
+    const editTask = task => {
+        return fetch(`http://localhost:8088/tasks/${task.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(task)
+        })
+            .then(getTasks)
+    }
+
+    /*
+        You return a context provider which has the
+        `locations` state, the `addLocation` function,
+        and the `getLocation` function as keys. This
+        allows any child elements to access them.
+    */
+    return (
+        <TaskContext.Provider value={{
+            tasks, getTasks, addTask, getTaskById, deleteTask, editTask
+        }}>
+            {props.children}
+        </TaskContext.Provider>
+    )
+}
