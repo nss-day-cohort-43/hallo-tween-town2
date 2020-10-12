877824015f36b584e71ae7803eb183b410ab5648
Abdurehman05 <abdukr5@gmail.com>
2020-10-12 11:14:45 -0500

    event card and provider updated

--- src/Components/ApplicationView.js
+++ src/Components/ApplicationView.js
@@ -1,16 +1,24 @@
-import React from "react"
-import { Route } from "react-router-dom"
-import { MessageList } from "./Messages/MessageList"
-import { MessageProvider } from "./Messages/MessageProvider"
+import React from "react";
+import { Route } from "react-router-dom";
+import { MessageList } from "./Messages/MessageList";
+import { MessageProvider } from "./Messages/MessageProvider";
+import { EventProvider } from "./event/EventProvider";
+import { EventCard } from "./event/EventCard";
 
-export const ApplicationViews = (props) => {
-    return (
-        <>
-            <MessageProvider>
-                <Route path="/">
-                    <MessageList />
-                </Route>
-            </MessageProvider>
-        </>
-    )
-}
+export const ApplicationViews = props => {
+  return (
+    <>
+      <MessageProvider>
+        <Route path="/">
+          <MessageList />
+        </Route>
+      </MessageProvider>
+
+      <EventProvider>
+        <Route path="/">
+          <EventCard />
+        </Route>
+      </EventProvider>
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom"
-import './index.css';
-import { App } from '../src/Components/App';
-import 'semantic-ui-css/semantic.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import { App } from "../src/components/App";
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,5 +11,5 @@ ReactDOM.render(
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
