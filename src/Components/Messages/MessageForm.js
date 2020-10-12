685e525c3de278b5d5d4386c4900037eb0420a95
Richie Holsenback <richieholsenback@gmail.com>
2020-10-12 11:27:40 -0400

    login page and add message working

--- src/Components/Messages/MessageForm.js
+++ src/Components/Messages/MessageForm.js
@@ -1,14 +1,83 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom'
+import React, { useContext, useState, useEffect } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
 import { Form } from 'semantic-ui-react'
+import { MessageContext } from './MessageProvider'
 
-export const MessageForm = () => (
-    <>
-        <Form>
-            <Form.Field>
-                <label>User Input</label>
-                <input />
-            </Form.Field>
-        </Form>
-    </>
-)
+export const MessageForm = (props) => {
+    const { addMessage, getMessageById, updateMessage } = useContext(MessageContext)
+
+    const [message, setMessage] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+
+    const { messageId } = useParams();
+    const history = useHistory()
+
+    const handleControlledInputChange = (event) => {
+        const newMessage = { ...message }
+        newMessage[event.target.name] = event.target.value
+        setMessage(newMessage)
+    }
+
+    useEffect(() => {
+        if (messageId) {
+            getMessageById(messageId)
+                .then(message => {
+                    setMessage(message)
+                    setIsLoading(false)
+                })
+        } else {
+            setIsLoading(false)
+        }
+    }, [])
+
+    const constructMessageObject = () => {
+        setIsLoading(true);
+        if (messageId) {
+            updateMessage({
+                id: parseInt(message.id),
+                message: message.message,
+                date: message.date,
+                userId: parseInt(localStorage.getItem("werewolf_user"))
+            })
+                .then(() => history.push(`/messages/detail/${message.id}`))
+        } else {
+            addMessage({
+                message: message.message,
+                date: message.date,
+                userId: parseInt(localStorage.getItem("werewolf_user"))
+            })
+                .then(() => history.push("messages"))
+        }
+    }
+
+    return (
+        <form className="messageForm">
+            <h2 className="messageForm__title">New Message</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="messageMessage">Type your message here: </label>
+                    <input type="text" id="messageMessage" name="message" required autoFocus className="form-control"
+                        placeholder="Message name"
+                        onChange={handleControlledInputChange}
+                        defaultValue={message.name} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="messageDate">Date of event: </label>
+                    <input type="date" id="messageDate" name="date" required className="form-control"
+                        onChange={handleControlledInputChange}
+                        defaultValue={message.date} />
+                </div>
+            </fieldset>
+            <button type="submit"
+                className="btn btn-primary"
+                disabled={isLoading}
+                onClick={event => {
+                    event.preventDefault() // Prevent browser from submitting the form
+                    constructMessageObject()
+                }}>
+                {messageId ? <>Save Message</> : <>Add Message</>}</button>
+        </form>
+    )
+}
