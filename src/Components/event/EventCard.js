a1c0c3e82784740c905806a4167787ae1c50ea68
Abdurehman05 <abdukr5@gmail.com>
2020-10-13 11:29:19 -0500

    dates in card formatted using moment library

--- src/Components/event/EventCard.js
+++ src/Components/event/EventCard.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Event.css";
 import { Link } from "react-router-dom";
+import moment from "moment";
 
 export const EventCard = ({ event }) => (
   <section className="event">
     <h3 className="event__name">
       <Link to={`events/detail/${event.id}`}>{event.name}</Link>
     </h3>
-    <div className="event__date">Date: {event.date} </div>
+    <div className="event__date">
+      <h4>
+        Date: {moment(event.date).format("MMMM Do YYYY")}--
+        {moment(event.date)
+          .startOf("day")
+          .fromNow()}
+      </h4>
+    </div>
     {/* <div className="event__location">Location:{event.location}</div> */}
     <br></br>
   </section>
