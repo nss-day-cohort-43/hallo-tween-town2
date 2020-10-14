fd1c45893310130b16ca82a4b374e99dcfc3699b
hrissolo <hrissolo@gmail.com>
2020-10-14 09:29:15 -0500

    css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.js"
 import {Image} from 'semantic-ui-react'
 import "./App.css"
+import logo from "./img/moonLogo.png"
 
 export const Home = () => (
     <>
         <div className="homeTitle">
-            <Image src="/src/moonLogo.png" alt="hallo-tweens"/>
+            <Image src={logo} alt="hallo-tweens"/>
         <h2>HalloTWEEN Town Werewolves</h2>
         <small>Catch us outside ...howling</small>
         </div>
